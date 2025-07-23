7cd88a68c73e558c60a696530c122a151ff03026
Shivam_bhawel <shivambhavel304@gmail.com>
2025-07-23 19:57:11 +0530

    server file updated

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-require('dotenv').config();
 const mongoose = require('mongoose');
 const { Server } = require('socket.io');
 const jwt = require('jsonwebtoken');
@@ -19,14 +18,15 @@ app.use(express.json());
 app.use('/user' , UserRoutes);
 
 // Connect DB
-mongoose.connect(process.env.MONGO_URI);
-const db = mongoose.connection;
-   db.on('connected' , () => {
-    console.log('database connected');
-   })
-   db.on('error' , (err) => {
-    console.log('database connection error',err);
-   })
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ Database connected'))
+.catch((err) => console.error('❌ Database connection error:', err));
+
 
 // SOCKET.IO
 io.on('connection', async (socket) => {
